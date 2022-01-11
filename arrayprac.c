11d9b9c2890f7fd0f14a49c5228215957443f4b9
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-11 17:58:58 +0530

    dsa practice

--- arrayprac.c
+++ arrayprac.c
@@ -7,103 +7,148 @@ struct Array{
     int length;
 };
 
-void displayArray(struct Array array){
-    for(int i=0;i<array.length;i++ ){
-        printf("Element %d\n",array.A[i]);
-    };
-};
-void append(struct Array *array,int value){
-    array->A[array->length]=value;
-    array->length++;
-};
-void insertAtIndex(struct Array *array, int index, int value){
-    for(int i=array->length;i>=index;i--){
-        array->A[i]=array->A[i-1];
-};
-    array->A[index]=value;
-    array->length++;
-};
-void deleteElement(struct Array *array,int index){
-    for(int i=index;i<array->length;i++)
-        array->A[i]=array->A[i+1];
-    array->length--;
-};
-
-void swap(struct Array *arr,int *x,int *y){         //transpostition 'Improving Linear Search'
-        int temp = *x;
-        *x = *y;
-        *y = temp;
-};
-
-int binarySearch(struct Array array,int key){ // array should be sorted when using binary
-    int l,h,m;
-    l=0;
-    h=array.length-1;
-    while(l<=h){
-        m=(l+h)/2;
-        if(array.A[m]==key)
-            return m;
-        else if(key<array.A[m])
-            h=m-1;
-        else 
-            l=m+1;
+void optionSelect(struct Array *array){
+    int n;
+    int inx,val;
+    printf("1. Display Array \n2. Insert value at Index \n3. Delete Element at Index \n4. Linear Search \n5. Binary Search \n6. Recursive Binary Search \n7. Exit \n");
+    scanf("%d",&n);
+    if(n==2){
+        printf("Enter Value and Index \n");
+        scanf("%d %d",&val,&inx);
     }
-    return -1;
+    else if(n==3){
+        printf("Enter Index to be deleted \n");
+        scanf("%d",&inx);
+    }
+    else if(n==4){
+        printf("Enter the value to be searched \n");
+        scanf("%d",&val);
+    }
+    else if(n==5 || n==6){
+        printf("Enter the Value to be searched, it'll work properly if the array is sorted otherwise use linear Search\n");
+        scanf("%d",&val);
+    }
+    switch (n)
+    {
+    case (1):
+        displayArray(array);
+        break;
+    case (2):
+        insertAt(array,inx,val);
+        break;
+    case (3):
+        deleteAt(array,inx);
+        break;
+    case (4):
+        linearSearch(array,val);
+        break;
+    case (5):
+        binarySearch(array,val);
+        break;
+    case (6):
+        printf("Value is %d\n",recursiveBinarySeach(array,val,0,array->length-1));
+        optionSelect(array);
+        break;
+    case (7)://break out
+        break;
+    }
+}
+
+int displayArray(struct Array *array){
+    for(int i=0;i<array->length;i++){
+        printf("Element %d\n",array->A[i]);
+    }
+    optionSelect(array);
 };
 
-int recursiveBinarySearch(struct Array array,int l,int h,int key){
-    int m=0;
-    if(l<=h){
-        m=(h+l)/2;
-        if(key==array.A[m])
-            return m;
-        else if(key<array.A[m])
-            return recursiveBinarySearch(array,l,m-1,key);
-        else
-            return recursiveBinarySearch(array,m+1,h,key);
+int insertAt(struct Array *array,int index,int value){
+    if(array->length<array->size){
+        for(int i=array->length;i>=index;i--){
+            array->A[i]=array->A[i-1];
+        }
+        array->A[index] = value;
+        array->length++;
+    }
+    optionSelect(array);
+};
+
+int deleteAt(struct Array *array,int index){
+    for(int i=index;i<array->length;i++){
+        array->A[i]=array->A[i+1];
+    }
+    array->length--;
+    optionSelect(array);
+};
+
+int swap(struct Array *array,int current,int previous){
+    if(current!=0){
+        printf("Using Transposition shifting element to left by one index \n");
+        int temp = array->A[previous];
+        array->A[previous] = array->A[current];
+        array->A[current] = temp;
+    }
+    optionSelect(array); 
+}
+
+int linearSearch(struct Array *array,int value){
+    for(int i=0;i<array->length;i++){
+        if(array->A[i]==value){   
+            printf("%d is at index: %d\n",value,i);        
+            swap(array,i,i-1);
+            break;
+        }
     }
     return -1;
 }
 
-int linearSearch(struct Array *array,int key){
-    for(int i=0;i<array->length;i++){
-        if(array->A[i]==key){
-            swap(array,&array->A[i],&array->A[i-1]);
-            return i;
+int binarySearch(struct Array *array,int value){
+    int l,h,mid;
+    l=0;
+    h=array->length-1;
+    while(l<=h){
+        mid=l+h/2;
+        if(array->A[mid]==value){
+            printf("Element is at Index :%d\n",mid);
+            break;
         }
+        else if(array->A[mid]>value)
+        h = mid-1;
+        else if(array->A[mid]<value)
+        l = mid+1;
     }
+    optionSelect(array);
+}
+
+int recursiveBinarySearch(struct Array *array,int value,int l,int h){
+    int mid;
+    mid=l+h/2;
+        if(array->A[mid]==value){
+            printf("Element is at Index :%d\n",mid);
+            return mid;
+        }
+        else if(array->A[mid]>value){
+            recursiveBinarySeach(array,value,l,mid-1);
+        }
+        else if(array->A[mid]<value){
+            recursiveBinarySeach(array,value,mid+1,h);
+        }
     return -1;
-};
+}
 
 int main(){
     struct Array arr;
-    int n,val;
-    printf("Enter the Size of Array");
+    printf("Enter the size of array \n");
     scanf("%d",&arr.size);
-    arr.A = (int *)malloc(arr.size);
-    printf("Enter No. of Elements");
+    printf("Enter the length of array \n");
     scanf("%d",&arr.length);
-    for(int i=0; i<arr.length;i++){
+    while(arr.length>arr.size){
+        printf("Length can't be greater than size, length is set to %d \n",arr.size-1);
+        arr.length = arr.size-1;
+    }
+    arr.A = (int *)malloc(arr.length);
+    printf("Enter elements in sorted manner (Binary Search) \n");
+    for(int i=0;i<arr.length;i++){
         scanf("%d",&arr.A[i]);
     }
-    append(&arr, 10);
-    printf("Enter value to be Inserted and index");
-    scanf("%d %d",&val,&n);
-    insertAtIndex(&arr,n,val);
-    displayArray(arr);
-    printf("Enter the index which should be deleted.");
-    scanf("%d",&n);
-    deleteElement(&arr,n);
-    displayArray(arr);
-    printf("Enter key to find in array using recursive binary search");
-    scanf("%d",&n);
-    printf("%d",recursiveBinarySearch(arr,0,arr.length-1,n));
-    // printf("Enter element to search");
-    // scanf("%d",&n);
-    // printf("The element is at index %d\n",linearSearch(&arr,n));
-    // displayArray(arr);
-    // printf("Enter key to find in array using binary search");
-    // scanf("%d",&n);
-    // printf("%d",binarySearch(arr,n));
-    return 0;
+    optionSelect(&arr);
 }
