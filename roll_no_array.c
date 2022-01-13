3b1f018ac03f7b893a5af60b3b6b7908379b13b5
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-13 14:09:26 +0530

    array practice roll no

--- roll_no_array.c
+++ roll_no_array.c
@@ -1,3 +1,5 @@
+// In an examination hall, seat number started from 0 then 1,2,3,.... and so on. English paper and Hindi paper students sitting on alternate seats with English paper students starting from seat 0, then Hindi paper student from seat 1 and so on. 
+//No two students with the same paper will sit together. There are N seats in a single row. Help the examiner to find the roll number of only English paper students in a particular row.
 #include <stdio.h>
 #include <stdlib.h>
 
