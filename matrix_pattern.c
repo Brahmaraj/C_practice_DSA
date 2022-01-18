b348eea8d53a68979ee31440830a96f7c1336c52
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-18 10:12:34 +0530

    matrix pattern

---
+++ matrix_pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Arr {
+    int *arr;
+    int dimension;
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    struct Arr array;
+    while(n--){
+        scanf("%d",&array.dimension)
+    }
+    return 0;
+}
