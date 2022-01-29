6316680eabc1f82071e3ab51f51896112300fde2
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-29 12:58:13 +0530

    binary numbers

--- binary_numbers.c
+++ binary_numbers.c
@@ -1,27 +1,28 @@
+// Problem Statement
+// Ninja is studying binary numbers and he likes binary numbers with maximum length.
+// He is having an array ‘A’ containing ‘N’ positive integers. He multiplies all the array elements and finds the maximum length binary number which is not greater than this product.
+// Can you tell the maximum length Ninja calculated.
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(){
-    int iter;
-    scanf("%d",&iter);
-    while (iter--)
-    {
-        int count=0,n=1,numbers,prod=1, *arr;
-        scanf("%d",&numbers);
-        arr = (int *)malloc(numbers * sizeof(int));
-        for(int i=0;i<numbers;i++){
-            scanf("%d",arr[i]);
+    int n;
+    scanf("%d",&n);
+    while(n--){
+        int no_of_elements=0, *elements=NULL,prod=1,cmp=1,count=0;
+        scanf("%d",&no_of_elements);
+        elements = (int *)malloc(no_of_elements*sizeof(int));
+        for(int i=0;i<no_of_elements;i++){
+            scanf("%d",&elements[i]);
         }
-        for(int i=0;i<numbers;i++){
-            prod = prod * arr[i];
+        for(int j=0;j<no_of_elements;j++){
+            prod = prod * elements[j];
         }
-        while(prod>=n){
-            n = n*2;
+        while(prod>=cmp){
+            cmp = 2*cmp;
             count++;
         }
-        printf("%d",count);
+        printf("%d\n",count);
     }
-    
-
-    
 }
