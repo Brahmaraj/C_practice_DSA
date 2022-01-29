4d201236c0e850c9def525bcbd6ac4fd00561cd3
Brahmaraj <brahmarajrathod@gmail.com>
2022-01-29 12:47:44 +0530

    binary no

--- binary_numbers.c
+++ binary_numbers.c
@@ -2,13 +2,26 @@
 #include <stdlib.h>
 
 int main(){
-    int n,bin_num;
-    int count=0;
-    n=1;
-    scanf("%d",&bin_num);
-    while(bin_num>=n){
-        n = n*2;
-        count++;
+    int iter;
+    scanf("%d",&iter);
+    while (iter--)
+    {
+        int count=0,n=1,numbers,prod=1, *arr;
+        scanf("%d",&numbers);
+        arr = (int *)malloc(numbers * sizeof(int));
+        for(int i=0;i<numbers;i++){
+            scanf("%d",arr[i]);
+        }
+        for(int i=0;i<numbers;i++){
+            prod = prod * arr[i];
+        }
+        while(prod>=n){
+            n = n*2;
+            count++;
+        }
+        printf("%d",count);
     }
-    printf("%d",count);
+    
+
+    
 }
