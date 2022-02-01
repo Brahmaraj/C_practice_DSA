78e1f36ca47a0ea3887bf8a9234fe0034d354479
Brahmaraj <brahmarajrathod@gmail.com>
2022-02-01 12:12:10 +0530

    bitmaksing

--- finding_duplicate_in_string_bitmasking.c
+++ finding_duplicate_in_string_bitmasking.c
@@ -6,10 +6,15 @@ int main(){
     int H=0,x=0;
     for(int i=0;A[i]!='\0';i++){
         x=1;
-        x=x<<(A[i]-97);
-        if((x&H)>0)
+        x=x<<(A[i]-97); // shifting x to the same value as x so the we can use it to mask or merge
+        if((x&H)>0) // masking is carried on (checking if the value is already present or not i.e merged)
         printf("%c is duplicate\n",A[i]);
         else
-            H=x|H;
+            H=x|H; // if the value is not present then value is merged 
     }
+    int y =5;
+    y = y<<1;  //left shift to multiply
+    printf("%d\n",y);
+    y =y>>1; //right shift to divide
+    printf("%d\n",y);
 }
